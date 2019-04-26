7a4b6602f15650594d5d1ee44c3ec75932c2af8b
Balázs Rigó <rigo.balazs86@gmail.com>
2019-04-26 22:40:30 +0200

    Fundamental Binary Tree implemented

---
+++ DataStructures/DataStructures/FundamentalsPDF/BinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.FundamentalsPDF
+{
+    class BinaryTree<T>
+    {
+        public T Value { get; set; }
+
+        public BinaryTree<T> LeftChild { get; private set; }
+        public BinaryTree<T> RightChild { get; private set; }
+
+        public BinaryTree(T value, BinaryTree<T> leftChild, BinaryTree<T> rightChild)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public BinaryTree(T value) : this(value, null, null)
+        {
+        }
+
+        public void PrintInOrder()
+        {
+            // 1. Visit the left child
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.PrintInOrder();
+            }
+
+            // 2. Visit the root of this sub-tree
+            Console.WriteLine(this.Value + " ");
+
+            // 3. Visit the right child
+            if (this.RightChild != null)
+            {
+                this.RightChild.PrintInOrder();
+            }
+        }
+    }
+}
---
+++ DataStructures/DataStructures/Program.cs
@@ -0,0 +1,35 @@
+﻿using DataStructures.FundamentalsPDF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Fundamental Binary Tree
+            BinaryTree<int> binaryTree =
+new BinaryTree<int>(14,
+new BinaryTree<int>(19,
+new BinaryTree<int>(23),
+new BinaryTree<int>(6,
+new BinaryTree<int>(10),
+new BinaryTree<int>(21))),
+new BinaryTree<int>(15,
+new BinaryTree<int>(3),
+null));
+            // Traverse and print the tree in in-order manner
+            binaryTree.PrintInOrder();
+            Console.WriteLine();
+
+            // Console output:
+            // 23 19 10 6 21 14 3 15
+
+            #endregion
+        }
+    }
+}
