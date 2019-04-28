eb5dc0d3d9fcb72c2a8e98c414267b700a8fcf02
Balázs Rigó <rigo.balazs86@gmail.com>
2019-04-28 20:15:10 +0200

    binary search tree implemented from https://codereview.stackexchange.com/questions/178145/generic-binary-search-tree-class-written-in-c

---
+++ DataStructures/DataStructures/FromNet/BinNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.FromNet
+{
+    class BinNode<T> where T:IComparable
+    {
+        public BinNode<T> right { get; set; }
+        public BinNode<T> left { get; set; }
+        public T data { get; set; }
+
+        public BinNode(T data)
+        {
+            this.data = data;
+        }
+    }
+}
